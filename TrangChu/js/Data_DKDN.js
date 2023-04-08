04aca2f296de2ae81712e8451761292f1db6f016
NguyenTrongTinhh <nguyentrongtinh4803@gmail.com>
2023-04-09 04:24:28 +0700

    add JS trang chu lan 1

---
+++ TrangChu/js/Data_DKDN.js
@@ -0,0 +1,9 @@
+let a = 
+{ 
+    sdt: "0988434215",
+    hoTen: "Phan Hiep",
+    matKhau: "0988434215Az",
+    xacNhanMatKhau: "0988434215Az"
+}
+let dsTK = [a]
+localStorage.setItem("TaiKhoan",JSON.stringify(dsTK))
