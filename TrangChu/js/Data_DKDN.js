ec11c92518c4d6d412437f638844060baae90ba0
NguyenTrongTinhh <nguyentrongtinh4803@gmail.com>
2023-04-10 16:36:41 +0700

    xoa JS bi loi lam lai

--- TrangChu/js/Data_DKDN.js
+++
@@ -1,9 +0,0 @@
-let a = 
-{ 
-    sdt: "0988434215",
-    hoTen: "Phan Hiep",
-    matKhau: "0988434215Az",
-    xacNhanMatKhau: "0988434215Az"
-}
-let dsTK = [a]
-localStorage.setItem("TaiKhoan",JSON.stringify(dsTK))
