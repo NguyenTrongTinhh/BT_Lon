02678711f1092c92c15c8c49b336096a324d2c66
NguyenTrongTinhh <nguyentrongtinh4803@gmail.com>
2023-05-04 10:00:10 +0700

    add js trang chu

---
+++ TrangChu/js/Data_DKDN.js
@@ -0,0 +1,9 @@
+let a = 
+{ 
+    sdt: "0989756661",
+    hoTen: "Trong Tinh",
+    matKhau: "123456789aZ",
+    xacNhanMatKhau: "0123456789aZ"
+}
+let dsTK = [a]
+localStorage.setItem("TaiKhoan",JSON.stringify(dsTK))
---
+++ TrangChu/js/Data_cuaHang.js
@@ -0,0 +1,347 @@
+let loai1 = {
+    maLoai: 1,
+    tenLoai: "Áo thun"
+}
+let loai2 = {
+    maLoai: 2,
+    tenLoai: "Áo sơ mi"
+}
+let loai3 = {
+    maLoai: 3,
+    tenLoai: "Quần"
+}
+let loai4 = {
+    maLoai: 4,
+    tenLoai: "Giày"
+}
+
+let dsLoaiSP = [loai1, loai2, loai3, loai4]
+localStorage.setItem("dsLoaiSP", JSON.stringify(dsLoaiSP));
+
+let sp1 = {
+    masp: 1,
+    tensp: "Áo thun Crew",
+    maLoai: 1,
+    hinhanh: "../img/aoT1.jpg",
+    soluong: 10,
+    dongia: 120000
+}
+let sp2 = {
+    masp: 2,
+    tensp: "Áo thun Marvel",
+    maLoai: 1,
+    hinhanh: "../img/aoT2.jpg",
+    soluong: 15,
+    dongia: 150000
+}
+let sp3 = {
+    masp: 3,
+    tensp: "Áo thun trắng",
+    maLoai: 1,
+    hinhanh: "../img/aoT3.jpg",
+    soluong: 20,
+    dongia: 175000
+}
+let sp4 = {
+    masp: 4,
+    tensp: "Áo thun tay lỡ mặt nạ",
+    maLoai: 1,
+    hinhanh: "../img/aoT4.jpg",
+    soluong: 30,
+    dongia: 155000
+}
+let sp5 = {
+    masp: 5,
+    tensp: "Áo thun Mỹ",
+    maLoai: 1,
+    hinhanh: "../img/aoT5.jpg",
+    soluong: 25,
+    dongia: 185000
+}
+let sp6 = {
+    masp: 6,
+    tensp: "Áo thun đen",
+    maLoai: 1,
+    hinhanh: "../img/aothunT6.jpg",
+    soluong: 80,
+    dongia: 989000
+}
+let sp7 = {
+    masp: 7,
+    tensp: "Áo thun Đính Nhãn",
+    maLoai: 1,
+    hinhanh: "../img/aothunT7.jpg",
+    soluong: 12,
+    dongia: 799000
+}
+let sp8 = {
+    masp: 8,
+    tensp: "Áo thun cổ trụ xám đỏ",
+    maLoai: 1,
+    hinhanh: "../img/aothunT8.jpg",
+    soluong: 53,
+    dongia: 99000
+}
+let sp9 = {
+    masp: 9,
+    tensp: "Áo thun Squad 21",
+    maLoai: 1,
+    hinhanh: "../img/aothunT9.jpg",
+    soluong: 23,
+    dongia: 1125000
+}
+let sp10 = {
+    masp: 10,
+    tensp: "Áo thun cổ tròn đen",
+    maLoai: 1,
+    hinhanh: "../img/aothunT10.jpg",
+    soluong: 6,
+    dongia: 99000
+}
+
+let sp11 = {
+    masp: 11,
+    tensp: "Áo sơ mi trắng",
+    maLoai: 2,
+    hinhanh: "../img/aosmT11.jpg",
+    soluong: 6,
+    dongia: 450000
+}
+let sp12 = {
+    masp: 12,
+    tensp: "Áo sơ mi nam ngắn tay",
+    maLoai: 2,
+    hinhanh: "../img/aosmT12.jpg",
+    soluong: 2,
+    dongia: 599000
+}
+let sp13 = {
+    masp: 13,
+    tensp: "Áo Sơ Mi Nam Tay Dài Aristino Trắng",
+    maLoai: 2,
+    hinhanh: "../img/aosmT13.jpg",
+    soluong: 1,
+    dongia: 416500
+}
+let sp14 = {
+    masp: 14,
+    tensp: "Áo sơ mi nam tay lỡ",
+    maLoai: 2,
+    hinhanh: "../img/aosmT14.jpg",
+    soluong: 2,
+    dongia: 260000
+}
+let sp15 = {
+    masp: 15,
+    tensp: "Áo Sơ Mi Nam WhiteonBlue",
+    maLoai: 2,
+    hinhanh: "../img/aosmT15.jpg",
+    soluong: 5,
+    dongia: 765000
+}
+let sp16 = {
+    masp: 16,
+    tensp: "Áo sơ mi nam cổ trụ",
+    maLoai: 2,
+    hinhanh: "../img/aosmT16.jpg",
+    soluong: 7,
+    dongia: 309000
+}
+let sp17 = {
+    masp: 17,
+    tensp: "Áo sơ mi nam dài tay",
+    maLoai: 2,
+    hinhanh: "../img/aosmT17.jpg",
+    soluong: 100,
+    dongia: 89000
+}
+let sp18 = {
+    masp: 18,
+    tensp: "ÁO SƠ MI NAM",
+    maLoai: 2,
+    hinhanh: "../img/aosmT18.jpg",
+    soluong: 3,
+    dongia: 786000
+}
+let sp19 = {
+    masp: 19,
+    tensp: "Áo sơ mi nam kẻ sọc xanh dương",
+    maLoai: 2,
+    hinhanh: "../img/aosmT19.jpg",
+    soluong: 4,
+    dongia: 285000
+}
+let sp20 = {
+    masp: 20,
+    tensp: "Áo sơ mi Nam ngắn tay",
+    maLoai: 2,
+    hinhanh: "../img/aosmT20.jpg",
+    soluong: 10,
+    dongia: 199000
+}
+
+let sp21 = {
+    masp: 21,
+    tensp: "Quần jogger",
+    maLoai: 3,
+    hinhanh: "../img/quanT21.jpg",
+    soluong: 10,
+    dongia: 300000
+}
+let sp22 = {
+    masp: 22,
+    tensp: "Quần kaki",
+    maLoai: 3,
+    hinhanh: "../img/quanT22.jpg",
+    soluong: 15,
+    dongia: 450000
+}
+let sp23 = {
+    masp: 23,
+    tensp: "Quần 3 sọc",
+    maLoai: 3,
+    hinhanh: "../img/quanT23.jpg",
+    soluong: 50,
+    dongia: 100000
+}
+let sp24 = {
+    masp: 24,
+    tensp: "Quần 3 sọc",
+    maLoai: 3,
+    hinhanh: "../img/quanT24.jpg",
+    soluong: 50,
+    dongia: 110000
+}
+let sp25 = {
+    masp: 25,
+    tensp: "Quần 3 sọc",
+    maLoai: 3,
+    hinhanh: "../img/quanT25.jpg",
+    soluong: 50,
+    dongia: 120000
+}
+let sp26 = {
+    masp: 26,
+    tensp: "Quần thể thao",
+    maLoai: 3,
+    hinhanh: "../img/quanT26.jpg",
+    soluong: 70,
+    dongia: 280000
+}
+let sp27 = {
+    masp: 27,
+    tensp: "Quần thể thao",
+    maLoai: 3,
+    hinhanh: "../img/quanT27.jpg",
+    soluong: 70,
+    dongia: 300000
+}
+let sp28 = {
+    masp: 28,
+    tensp: "Quần thể thao ngắn",
+    maLoai: 3,
+    hinhanh: "../img/quanT28.jpg",
+    soluong: 30,
+    dongia: 280000
+}
+let sp29 = {
+    masp: 29,
+    tensp: "Quần thể thao ngắn",
+    maLoai: 3,
+    hinhanh: "../img/quanT29.jpg",
+    soluong: 30,
+    dongia: 280000
+}
+let sp30 = {
+    masp: 30,
+    tensp: "Quần thể thao ngắn",
+    maLoai: 3,
+    hinhanh: "../img/quanT30.jpg",
+    soluong: 30,
+    dongia: 200000
+}
+
+let sp31 = {
+    masp: 31,
+    tensp: "Giày da Chelsea Boot",
+    maLoai: 4,
+    hinhanh: "../img/giayT31.jpg",
+    soluong: 10,
+    dongia: 750000
+}
+let sp32 = {
+    masp: 32,
+    tensp: "Giày BOSTON",
+    maLoai: 4,
+    hinhanh: "../img/giayT32.jpg",
+    soluong: 14,
+    dongia: 150000
+}
+let sp33 = {
+    masp: 33,
+    tensp: "Giày nam Versace",
+    maLoai: 4,
+    hinhanh: "../img/giayT33.jpg",
+    soluong: 20,
+    dongia: 250000
+}
+let sp34 = {
+    masp: 34,
+    tensp: "Giày sneaker nam",
+    maLoai: 4,
+    hinhanh: "../img/giayT34.jpg",
+    soluong: 40,
+    dongia: 200000
+}
+let sp35 = {
+    masp: 35,
+    tensp: "Giày Sneaker Sodoha",
+    maLoai: 4,
+    hinhanh: "../img/giayT35.jpg",
+    soluong: 32,
+    dongia: 650000
+}
+let sp36 = {
+    masp: 36,
+    tensp: "GiàySneaker cao cổ ",
+    maLoai: 4,
+    hinhanh: "../img/giayT36.jpg",
+    soluong: 11,
+    dongia: 180000
+}
+let sp37 = {
+    masp: 37,
+    tensp: "Giày Gucci Screener",
+    maLoai: 4,
+    hinhanh: "../img/giayT37.jpg",
+    soluong: 25,
+    dongia: 350000
+}
+let sp38 = {
+    masp: 38,
+    tensp: "Giày MLB Dodgers LA",
+    maLoai: 4,
+    hinhanh: "../img/giayT38.jpg",
+    soluong: 34,
+    dongia: 430000
+}
+let sp39 = {
+    masp: 39,
+    tensp: "Giày Vans",
+    maLoai: 4,
+    hinhanh: "../img/giayT39.jpg",
+    soluong: 16,
+    dongia: 500000
+}
+let sp40 = {
+    masp: 40,
+    tensp: "Giày Nike Air",
+    maLoai: 4,
+    hinhanh: "../img/giayT40.jpg",
+    soluong: 37,
+    dongia: 550000
+}
+
+let dssp = [sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9, sp10, sp11, sp12, sp13, sp14, sp15, sp16, sp17, sp18, sp19, sp20, sp21, sp22, sp23, sp24, sp25, sp26, sp27, sp28, sp29, sp30, sp31, sp32, sp33, sp34, sp35, sp36, sp37, sp38, sp39, sp40]
+
+localStorage.setItem("dssp", JSON.stringify(dssp));
