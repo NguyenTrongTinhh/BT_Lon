538a7b59a1c4dd17ab514204e72640eaacae25d3
NguyenTrongTinhh <nguyentrongtinh4803@gmail.com>
2023-05-04 12:54:11 +0700

    add js gioi thieu lan 2

--- GioiThieu/js/jsGT.js
+++ GioiThieu/js/jsGT.js
@@ -4,4 +4,9 @@ window.onload = function () {
         document.querySelector("#khongDN").style.display = "none";
         document.querySelector("#sauDN").style.display = "block";
     }
+    $("#dangxuat").click(function(){
+        document.querySelector("#khongDN").style.display = "block";
+        document.querySelector("#sauDN").style.display = "none";
+        localStorage.setItem("flag",false)
+    })
 }
